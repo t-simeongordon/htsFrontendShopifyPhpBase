3b1ad23cafc9a4c0e118d93ccaa9975e39ba171e
tsg <t.simeongordon@gmail.com>
2024-08-16 15:37:33 +0100

    v0: install - nonce value added with generated bytes value

--- install.php
+++ install.php
@@ -5,5 +5,5 @@ $_NGROK_URL='https://4530-2a02-c7c-a460-e000-e9d7-294a-8fe-2d03.ngrok-free.app';
 $shop=$_GET['shop'];
 $scopes='read_products,write_products,read_orders,write_orders';
 $redirect_uri=$_NGROK_URL . '/php-app/token.php'
-
+$nonce=random_bytes(12)
 echo $shop
