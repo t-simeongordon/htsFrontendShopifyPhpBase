b19abb7ff62c65bbd2055382bba4ac86f027c34e
tsg <t.simeongordon@gmail.com>
2024-08-16 15:41:20 +0100

    v0: install - access mode added

--- install.php
+++ install.php
@@ -4,6 +4,7 @@ $_API_KEY='';
 $_NGROK_URL='https://4530-2a02-c7c-a460-e000-e9d7-294a-8fe-2d03.ngrok-free.app';
 $shop=$_GET['shop'];
 $scopes='read_products,write_products,read_orders,write_orders';
-$redirect_uri=$_NGROK_URL . '/php-app/token.php'
-$nonce=bin2hex(random_bytes(12))
+$redirect_uri=$_NGROK_URL . '/php-app/token.php';
+$nonce=bin2hex(random_bytes(12));
+$access_mode='per-user';
 echo $shop
