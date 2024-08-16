269c3117324ff6436c5164badb5a80d3b5f7531e
tsg <t.simeongordon@gmail.com>
2024-08-16 14:03:14 +0100

    v0: install - redirect_uri added

--- install.php
+++ install.php
@@ -1,7 +1,9 @@
 <?php
 
-$_API_KEY=''
-$_NGROK_URL='https://d0d7-2a02-c7c-a460-e000-a18d-7949-46e1-7ee5.ngrok-free.app'
-$shop=$_GET['shop']
+$_API_KEY='';
+$_NGROK_URL='https://4530-2a02-c7c-a460-e000-e9d7-294a-8fe-2d03.ngrok-free.app';
+$shop=$_GET['shop'];
+$scopes='read_products,write_products,read_orders,write_orders';
+$redirect_uri=$_NGROK_URL . '/php-app/token.php'
 
 echo $shop
